7e2eface5919e522daca1335d726f360451c3328
Christopher Speltz <chrisspeltz@msn.com>
2022-01-14 22:24:42 -0600

    modified index database model

--- models/index.js
+++ models/index.js
@@ -1,31 +1,43 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'user_id'
+  foreignKey: "category_id",
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'user_id'
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'voted_posts',
-  foreignKey: 'post_id'
+  as: "tag_product",
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'voted_posts',
-  foreignKey: 'post_id'
+  as: "tag_product",
+  foreignKey: "tag_id",
 });
+
+// User.belongsToMany(Post, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'user_id'
+// });
+
+// Post.belongsToMany(User, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'post_id'
+// });
 
 module.exports = {
   Product,
